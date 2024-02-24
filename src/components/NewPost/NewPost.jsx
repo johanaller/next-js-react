99ed45f0c9d6c8ffe1ac3438a8b52e942c85c3ab
johanaller <johan.berglund@aller.com>
2024-02-24 15:44:00 +0100

    starting on form submission

--- src/components/NewPost/NewPost.jsx
+++ src/components/NewPost/NewPost.jsx
@@ -1,22 +1,52 @@
 import styles from "./NewPost.module.css"
+import { useState } from "react";
 
-function NewPost({ onAuthorChange, onBodyChange }) {
-    return (
-        <form className={styles.form}>
-            <p>
-                <label htmlFor="name"> Your name</label>
-                <input type="text" id="name" placeholder="Ett placeholder namn..." required onChange={onAuthorChange} />
-            </p>
-            <p>
-                <label htmlFor="body">Text</label>
-                <textarea 
-                    id="body" 
-                    required rows={3} 
-                    placeholder={'Det här är en placeholder...'} 
-                    onChange={onBodyChange} />
-            </p>
-        </form>
-    )
-}
+function NewPost({  onSubmit }) {
+   
+    const [authorName, setAuthorName] = useState("");
+    const [bodyText, setBodyText] = useState("");
+
+    const authorChangeHandler = (event) => {
+        setAuthorName(event.target.value)
+    };
+
+    const bodyChangeHandler = (event) => {
+        setBodyText(event.target.value)
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const postData = ({
+            author: authorName,
+            body: bodyText
+        });
+
+        console.log(postData)
+        setAuthorName("")
+        setBodyText("")
+        console.log(bodyText, authorName)
+    }
+
+        return (
+            <form className={styles.form} onSubmit={handleSubmit}>
+                <p>
+                    <label htmlFor="name"> Your name</label>
+                    <input type="text" id="name" placeholder="Ett placeholder namn..." required onChange={authorChangeHandler} />
+                </p>
+                <p>
+                    <label htmlFor="body">Text</label>
+                    <textarea
+                        id="body"
+                        required rows={3}
+                        placeholder={'Det här är en placeholder...'}
+                        onChange={bodyChangeHandler} />
+                </p>
+                <button type="button"></button>
+                <button type="submit">Create post </button>
+
+            </form>
+        )
+    }
 
 export default NewPost
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,30 +1,17 @@
 import NewPost from "../NewPost/NewPost";
 import Post from "../Post/Post";
 import styles from "./PostList.module.css"
-import { useState } from "react";
 import Modal from "../Modal/Modal";
 
 function PostList( {isModalOpen, onCloseModal}) {
-    
-    const [authorName, setAuthorName] = useState("");
-    const [bodyText, setBodyText] = useState("");
-
-    const authorChangeHandler = (event) => {
-        setAuthorName(event.target.value)
-    };
-
-    const bodyChangeHandler = (event) => {
-        setBodyText(event.target.value)
-    };
-
 
     return (
         <>
             { isModalOpen &&<Modal isOpen={isModalOpen} onClose={onCloseModal} >
-                <NewPost onAuthorChange={authorChangeHandler} onBodyChange={bodyChangeHandler} />
+            <NewPost  />
             </Modal>}
             <ul className={styles.posts}>
-                <Post author={authorName} body={bodyText} />
+                <Post  />
                 <Post author="bonny" body="says lets go" />
             </ul >
         </>
