551e3b1435f7f9dfb4008fd1ffe471104e5b47d1
johanaller <johan.berglund@aller.com>
2024-02-24 16:08:37 +0100

    updating button styling

--- src/components/NewPost/NewPost.jsx
+++ src/components/NewPost/NewPost.jsx
@@ -1,8 +1,8 @@
 import styles from "./NewPost.module.css"
 import { useState } from "react";
 
-function NewPost({  onSubmit }) {
-   
+function NewPost({ onClose }) {
+
     const [authorName, setAuthorName] = useState("");
     const [bodyText, setBodyText] = useState("");
 
@@ -14,7 +14,7 @@ function NewPost({  onSubmit }) {
         setBodyText(event.target.value)
     };
 
-    const handleSubmit = (event) => {
+    const submitHandler = (event) => {
         event.preventDefault();
 
         const postData = ({
@@ -23,30 +23,35 @@ function NewPost({  onSubmit }) {
         });
 
         console.log(postData)
-        setAuthorName("")
-        setBodyText("")
         console.log(bodyText, authorName)
+        onClose()
     }
 
-        return (
-            <form className={styles.form} onSubmit={handleSubmit}>
-                <p>
-                    <label htmlFor="name"> Your name</label>
-                    <input type="text" id="name" placeholder="Ett placeholder namn..." required onChange={authorChangeHandler} />
-                </p>
-                <p>
-                    <label htmlFor="body">Text</label>
-                    <textarea
-                        id="body"
-                        required rows={3}
-                        placeholder={'Det här är en placeholder...'}
-                        onChange={bodyChangeHandler} />
-                </p>
-                <button type="button"></button>
+    return (
+        <form className={styles.form} onSubmit={submitHandler}>
+            <p>
+                <label htmlFor="name"> Your name</label>
+                <input
+                    type="text"
+                    id="name"
+                    placeholder="Ett placeholder namn..."
+                    required onChange={authorChangeHandler} />
+            </p>
+            <p>
+                <label htmlFor="body">Text</label>
+                <textarea
+                    id="body"
+                    required rows={3}
+                    placeholder={'Det här är en placeholder...'}
+                    onChange={bodyChangeHandler} />
+            </p>
+            <p className={styles.actions}>
+                <button type="button">Cancel</button>
                 <button type="submit">Create post </button>
+            </p>
 
-            </form>
-        )
-    }
+        </form>
+    )
+}
 
 export default NewPost
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -3,15 +3,14 @@ import Post from "../Post/Post";
 import styles from "./PostList.module.css"
 import Modal from "../Modal/Modal";
 
-function PostList( {isModalOpen, onCloseModal}) {
+function PostList({ isModalOpen, onCloseModal }) {
 
     return (
         <>
-            { isModalOpen &&<Modal isOpen={isModalOpen} onClose={onCloseModal} >
-            <NewPost  />
+            {isModalOpen && <Modal isOpen={isModalOpen} onClose={onCloseModal} >
+                <NewPost onClose={onCloseModal} />
             </Modal>}
             <ul className={styles.posts}>
-                <Post  />
                 <Post author="bonny" body="says lets go" />
             </ul >
         </>
